dcfb9bec4034639222fae1ac06dfe883c00dee9e
Albert <albertbarnabas10@gmail.com>
2022-09-21 21:56:35 +0700

    new portfolio

--- src/pages/Home/portfolioData.js
+++ src/pages/Home/portfolioData.js
@@ -22,5 +22,13 @@ export const portfolioData = [
         description: 'FiPlanner is a Online Financial Planner Application where users can do Finance Planning by themselves or Through Expert Consultation.',
         role: 'UI Designer and Developer',
         link : '/'
+    },
+    {
+        image: 'JTravel',
+        title: 'JTravel',
+        type: 'UI Design',
+        description: 'JTravel is a website that provides information for Traveler outside of Japan.',
+        role: 'UI Designer',
+        link : 'https://www.behance.net/albertbarnabas10'
     }
 ]
